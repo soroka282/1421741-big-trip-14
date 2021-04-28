accfcd259301c0473f3a15516a987d14500b18b8
soroka282 <soroka282@yandex.ru>
2021-04-28 19:31:23 +0300

    Опять доработки

--- src/view/events.js
+++ src/view/events.js
@@ -26,7 +26,7 @@ const createEventsTemplate = (data) => {
     <div class="event">
       <time class="event__date" datetime=${getDateISO(dateFrom)}>${getDateMonthDay(dateFrom)}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${name}</h3>
       <div class="event__schedule">
